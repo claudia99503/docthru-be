5b5e9b65d783271b042d243cc3bb3d8d1fe9c18f
KEN <recom54@gmail.com>
2024-10-10 14:48:47 +0900

    :sparkles: [Feat] 유저기능 추가 (진행중)

---
+++ prisma/migrations/20241010003619_add_refresh_token/migration.sql
@@ -0,0 +1,140 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "UserGrade" AS ENUM ('NORMAL', 'EXPERT');
+
+-- CreateEnum
+CREATE TYPE "ChallengeField" AS ENUM ('NEXTJS', 'API', 'CAREER', 'MODERNJS', 'WEB');
+
+-- CreateEnum
+CREATE TYPE "DocType" AS ENUM ('OFFICIAL', 'BLOG');
+
+-- CreateEnum
+CREATE TYPE "ApplicationStatus" AS ENUM ('WAITING', 'ACCEPTED', 'REJECTED', 'DELETED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "grade" "UserGrade",
+    "nickName" VARCHAR(10) NOT NULL,
+    "email" VARCHAR(20) NOT NULL,
+    "password" VARCHAR(50) NOT NULL,
+    "refreshToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Challenge" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(200) NOT NULL,
+    "field" "ChallengeField" NOT NULL,
+    "docType" "DocType" NOT NULL,
+    "description" TEXT NOT NULL,
+    "docUrl" VARCHAR(200) NOT NULL,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "progress" BOOLEAN NOT NULL DEFAULT false,
+    "participates" INTEGER,
+    "maxParticipates" INTEGER,
+
+    CONSTRAINT "Challenge_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "challengeId" INTEGER NOT NULL,
+    "status" "ApplicationStatus" NOT NULL DEFAULT 'WAITING',
+    "aplliedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "invalidationComment" VARCHAR(200),
+    "invalidatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Participate" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "challengeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Participate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Work" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "challengeId" INTEGER NOT NULL,
+    "description" TEXT,
+    "lastModifiedAt" TIMESTAMP(3) NOT NULL,
+    "isSubmitted" BOOLEAN NOT NULL DEFAULT false,
+    "submittedAt" TIMESTAMP(3),
+    "likeCount" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Work_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "workId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Like" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "workId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nickName_key" ON "User"("nickName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Like_workId_key" ON "Like"("workId");
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "Challenge"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participate" ADD CONSTRAINT "Participate_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participate" ADD CONSTRAINT "Participate_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "Challenge"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Work" ADD CONSTRAINT "Work_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Work" ADD CONSTRAINT "Work_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "Challenge"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_workId_fkey" FOREIGN KEY ("workId") REFERENCES "Work"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_workId_fkey" FOREIGN KEY ("workId") REFERENCES "Work"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
