92f5787b71ba1ad8fad97b55012efe9a846bdaa7
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-10-15 10:19:54 +0900

    :recycle: [Refactor] work post 할 때 참가자, 이미 post 했는지 여부 체크

--- src/middlewares/authWorkMiddleware.js
+++ src/middlewares/authWorkMiddleware.js
@@ -23,3 +23,48 @@ export const authWorkAction = async (req, res, next) => {
     throw new UnauthorizedException('접근 권한이 없습니다');
   }
 };
+
+export const authCreateWorkAction = async (req, res, next) => {
+  const user = req.user;
+  const { challengeId } = req.params;
+
+  const challengeInfo = await prisma.challenge.findFirst({
+    where: {
+      id: Number(challengeId),
+    },
+    include: {
+      participations: true,
+      works: true,
+    },
+  });
+
+  const userInfo = await prisma.user.findUnique({
+    where: { id: Number(user.userId) },
+  });
+
+  if (!challengeInfo) {
+    return res.status(404).json({ message: '챌린지를 찾을 수 없습니다.' });
+  }
+
+  if (!userInfo) {
+    throw new UnauthorizedException('사용자 정보가 없습니다');
+  }
+
+  const isParticipating = challengeInfo.participations.some(
+    (participation) => participation.userId === userInfo.id
+  );
+
+  if (!isParticipating) {
+    throw new UnauthorizedException('접근 권한이 없습니다');
+  }
+
+  const hasSubmittedWork = challengeInfo.works.some(
+    (work) => work.userId === userInfo.id
+  );
+
+  if (hasSubmittedWork) {
+    return res.status(403).json({ message: '이미 작업을 제출하였습니다.' });
+  }
+
+  next();
+};
--- src/routes/workRoutes.js
+++ src/routes/workRoutes.js
@@ -1,22 +1,34 @@
 import express from 'express';
 import * as workController from '../controllers/workController.js';
 import { authenticateAccessToken } from '../middlewares/authMiddleware.js';
-import { authWorkAction } from '../middlewares/authWorkMiddleware.js';
+import {
+  authWorkAction,
+  authCreateWorkAction,
+} from '../middlewares/authWorkMiddleware.js';
 
 const router = express.Router();
 
-router.use(authenticateAccessToken);
+// router.use(authenticateAccessToken);
+// router.use(authWorkAction); 본인, 어드민만 수정, 삭제되게끔
+// router.use(authCreateWorkAction); 작성할 때 챌린지 참가 인원인지 체크
+
+// 워크 id에 userId가 있으면 접근 제한
 
 // 챌린지에 참가한 인원인지 권한 설정
-router.post('/:challengeId/post', workController.postWork);
+router.post(
+  '/:challengeId/post',
+  authenticateAccessToken,
+  authCreateWorkAction,
+  workController.postWork
+);
 
 router.post('/:workId/likes', workController.likeWork);
 
 //본인, 어드민 계정만 수정되게끔
-router.patch('/:workId/edit', authWorkAction, workController.editWork);
+router.patch('/:workId/edit', workController.editWork);
 
 //본인, 어드민 계정만 수정되게끔
-router.delete('/:workId/delete',authWorkAction, workController.deleteWork);
+router.delete('/:workId/delete', workController.deleteWork);
 
 router.delete('/:workId/likes', workController.likeCancelWork);
 
