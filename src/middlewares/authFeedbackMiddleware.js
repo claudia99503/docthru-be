f08a9537c3e3ef9605ee219300adb73b3e53abe6
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-10-15 22:52:20 +0900

    :recycle: [Refactor] 챌린지 마감 시 어드민 계정만 수정, 삭제

--- src/middlewares/authFeedbackMiddleware.js
+++ src/middlewares/authFeedbackMiddleware.js
@@ -10,27 +10,46 @@ export const autFeedbackAction = async (req, res, next) => {
   const user = req.user;
   const { feedbackId } = req.params;
 
-  const feedbackInfo = await prisma.feedback.findFirst({
-    where: {
-      id: Number(feedbackId),
-    },
-  });
+  try {
+    const feedbackInfo = await prisma.feedback.findFirst({
+      where: { id: Number(feedbackId) },
+      include: { work: true },
+    });
 
-  const userInfo = await prisma.user.findUnique({
-    where: { id: Number(user.userId) },
-  });
+    if (!feedbackInfo) {
+      return next(new NotFoundException('등록된 피드백이 없습니다.'));
+    }
 
-  if (!feedbackInfo) {
-    next(new NotFoundException('등록된 피드백이 없습니다.'));
-  }
+    const challengeInfo = await prisma.challenge.findFirst({
+      where: { id: feedbackInfo.work.challengeId },
+    });
 
-  if (!userInfo) {
-    next(new UnauthorizedException('사용자 정보가 없습니다'));
-  }
+    if (!challengeInfo) {
+      return next(new NotFoundException('등록된 챌린지가 없습니다.'));
+    }
 
-  if (userInfo.id === feedbackInfo.userId || userInfo.role === 'ADMIN') {
-    next();
-  } else {
-    next(new UnauthorizedException('접근 권한이 없습니다'));
+    const userInfo = await prisma.user.findUnique({
+      where: { id: Number(user.userId) },
+    });
+
+    if (!userInfo) {
+      return next(new UnauthorizedException('사용자 정보가 없습니다.'));
+    }
+
+    if (challengeInfo.progress) {
+      if (userInfo.role === 'ADMIN') {
+        return next();
+      } else {
+        return next(new UnauthorizedException('챌린지가 마감됐습니다.'));
+      }
+    }
+
+    if (userInfo.id === feedbackInfo.userId || userInfo.role === 'ADMIN') {
+      return next();
+    }
+
+    return next(new UnauthorizedException('접근 권한이 없습니다.'));
+  } catch (error) {
+    next(error);
   }
 };
