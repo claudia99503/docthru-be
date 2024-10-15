e1b364e88bc445a57fc2807ee6f9989f5cfdc5b4
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-10-15 09:03:54 +0900

    :recycle: [Refactor] 작성자, admin 계정 auth 수정

--- src/middlewares/authWorkMiddleware.js
+++ src/middlewares/authWorkMiddleware.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 export const authWorkAction = async (req, res, next) => {
   const user = req.user;
-  const { challengeId: workId } = req.params;
+  const { workId } = req.params;
 
   const workInfo = await prisma.work.findFirst({
     where: {
@@ -13,11 +13,13 @@ export const authWorkAction = async (req, res, next) => {
     },
   });
 
-  console.log('user :', user);
-  console.log('workInfo :', workInfo.userId);
-  // if (user.id !== workInfo.userId || user.UserRole !== ADMIN) {
-  //   throw new UnauthorizedException('접근 권한이 없습니다');
-  // }
+  const userInfo = await prisma.user.findUnique({
+    where: { id: Number(user.userId) },
+  });
 
-  next();
+  if (userInfo.id === workInfo.userId || userInfo.role === 'ADMIN') {
+    next();
+  } else {
+    throw new UnauthorizedException('접근 권한이 없습니다');
+  }
 };
--- src/routes/workRoutes.js
+++ src/routes/workRoutes.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import * as workController from '../controllers/workController.js';
 import { authenticateAccessToken } from '../middlewares/authMiddleware.js';
+import { authWorkAction } from '../middlewares/authWorkMiddleware.js';
 
 const router = express.Router();
 
 router.use(authenticateAccessToken);
 
+// 챌린지에 참가한 인원인지 권한 설정
 router.post('/:challengeId/post', workController.postWork);
+
 router.post('/:workId/likes', workController.likeWork);
 
-router.patch('/list/:workId', workController.editWork);
+//본인, 어드민 계정만 수정되게끔
+router.patch('/:workId/edit', authWorkAction, workController.editWork);
 
-router.delete('/:workId/delete', workController.deleteWork);
+//본인, 어드민 계정만 수정되게끔
+router.delete('/:workId/delete',authWorkAction, workController.deleteWork);
+
 router.delete('/:workId/likes', workController.likeCancelWork);
 
 router.get('/list/:challengeId', workController.worksList);
