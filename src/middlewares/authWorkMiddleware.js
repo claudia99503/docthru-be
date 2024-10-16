5c294bbcba45fc57c8e1828f32d55ce54dcbed7f
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-10-16 16:45:18 +0900

    :recycle: [Refactor] 중복된 오류메시지 수정

--- src/middlewares/authWorkMiddleware.js
+++ src/middlewares/authWorkMiddleware.js
@@ -87,26 +87,4 @@ export const authCreateWorkAction = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-
-  if (!userInfo) {
-    next(new UnauthorizedException('사용자 정보가 없습니다'));
-  }
-
-  const isParticipating = challengeInfo.participations.some(
-    (participation) => participation.userId === userInfo.id
-  );
-
-  if (!isParticipating) {
-    next(new UnauthorizedException('신청한 회원만 쓸 수 있습니다.'));
-  }
-
-  const hasSubmittedWork = challengeInfo.works.some(
-    (work) => work.userId === userInfo.id
-  );
-
-  if (hasSubmittedWork) {
-    next(new BadRequestException('이미 작업물을 등록했습니다.'));
-  }
-
-  next();
 };
