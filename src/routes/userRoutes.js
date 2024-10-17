ed09fc9caeb0736e86a76a6ac4465141fd25390e
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-10-17 16:26:07 +0900

    :recycle: [Refactor] user swagger 일부 추가

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,8 +4,67 @@ import { authenticateAccessToken } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: 사용자 관련 API
+ */
+
+/**
+ * @swagger
+ * /api/users/register:
+ *   post:
+ *     tags: [Users]
+ *     summary: 사용자 회원가입
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               nickname:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: 회원가입 성공
+ *       409:
+ *         description: 잘못된 전송입니다
+ */
+
 router.post('/register', userController.register);
+
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     tags: [Users]
+ *     summary: 사용자 로그인
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: 로그인 성공
+ *       401:
+ *         description: 로그인 실패
+ */
+
 router.post('/login', userController.login);
+
 router.post('/token/refresh', userController.refreshToken);
 
 router.use(authenticateAccessToken);
