36fbebfb04515b57fb344b3041f080217b9db1d4
KEN <recom54@gmail.com>
2024-10-17 15:37:22 +0900

    🔧 [Chore] 유저기능 요구사항관련

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,13 +7,12 @@ const router = Router();
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/token/refresh', userController.refreshToken);
+router.post('/logout', userController.logout);
+router.get('/me', userController.getCurrentUser);
 
 router.use(authenticateAccessToken);
 
-router.get('/me', userController.getCurrentUser);
-router.post('/logout', userController.logout);
 router.get('/:id', userController.getUserById);
-
 router.get('/me/challenges/ongoing', userController.getOngoingChallenges);
 router.get('/me/challenges/completed', userController.getCompletedChallenges);
 router.get('/me/challenges/applications', userController.getAppliedChallenges);
