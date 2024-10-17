fa486f17729e3825c6ed58f31a2adc4723c1496d
KEN <recom54@gmail.com>
2024-10-17 19:14:01 +0900

    🐛 [Fix] refreshToken 관련 수정

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import userRoutes from './routes/userRoutes.js';
-import workRoutes from './routes/workRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
+import { swaggerDocs } from './configs/swagger.js';
+import swaggerUi from 'swagger-ui-express';
+import customJsonParser from './middlewares/jsonParser.js';
+import { REFRESH_TOKEN_MAX_AGE } from './configs/config.js';
+
+import userRoutes from './routes/userRoutes.js';
+import workRoutes from './routes/workRoutes.js';
 import challengeRoutes from './routes/challengeRoutes.js';
 import applicationRoutes from './routes/applicationRoutes.js';
 import feedbackRoutes from './routes/feedbackRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
-import { swaggerDocs } from './configs/swagger.js';
-import swaggerUi from 'swagger-ui-express';
-import customJsonParser from './middlewares/jsonParser.js';
 
 dotenv.config();
 
@@ -36,15 +38,15 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(customJsonParser);
 
-// 쿠키 보안 설정
-app.use((req, res, next) => {
+export const sendRefreshToken = (res, token) => {
   res.cookie('refreshToken', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+    maxAge: parseInt(REFRESH_TOKEN_MAX_AGE, 10),
+    path: '/api/users/token/refresh',
   });
-  next();
-});
+};
 
 app.use(
   helmet.contentSecurityPolicy({
@@ -72,18 +74,16 @@ app.use(
   })
 );
 
+// Swagger 설정
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/api', notificationRoutes);
-app.use('/api/challenges', challengeRoutes);
+
+// API 라우트 설정
 app.use('/api/users', userRoutes);
 app.use('/api/works', workRoutes);
-app.use('/api/feedbacks', feedbackRoutes);
+app.use('/api/challenges', challengeRoutes);
 app.use('/api/applications', applicationRoutes);
-
-// 테스트용 엔드포인트 추가
-app.get('/api/test-cookie', (req, res) => {
-  res.json({ message: 'Cookie test successful', cookies: req.cookies });
-});
+app.use('/api/feedbacks', feedbackRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.use(errorHandler);
 
@@ -91,3 +91,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
   console.log(`Server is running on port http://localhost:${PORT}`)
 );
+
+export default app;
