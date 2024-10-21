5969ef554b0fd4255400710e5ca6fc3820983de5
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-10-21 09:38:31 +0900

    :recycle: [Refactor] swagger 3001번 연결 시  Not allowed by CORS 오류로 인한 수정

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 const allowedOrigins = [
   CLIENT_URL,
   'http://localhost:3000',
+  'http://localhost:3001',
   'https://vercel.live',
 ];
 
