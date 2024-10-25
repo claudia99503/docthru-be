fc2c7f025548f8a26fe6202cd09468dbd5e091f2
KEN <recom54@gmail.com>
2024-10-25 09:23:28 +0900

    🐛 [Fix] 마이그레이션 관련

--- prisma/migrations/20241025002249_docthru/migration.sql
+++ prisma/migrations/20241025002249_docthru/migration.sql
@@ -25,6 +25,8 @@ CREATE TABLE "User" (
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "refreshToken" TEXT,
+    "tokenUpdatedAt" TIMESTAMP(3),
+    "lastLoginAt" TIMESTAMP(3),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "bestCount" INTEGER NOT NULL DEFAULT 0,
